f2e4eb6e87bdbbfd4807df5d3e8a2892b2e4ffcf
srinadh1304 <srinadh1304@gmail.com>
2021-09-05 21:57:00 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hashtable;
+import com.bridgelabz.linkedlist.*;
+public class HashTableMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to hash table program");
+	}
+}
